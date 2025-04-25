4b24a665d2337c64cd8f57ccd3d0178b6a489868
Bang-jun00 <qudwns5250@naver.com>
2025-04-25 12:34:39 +0900

    [Feat] 두수의 나눗셈

---
+++ 입문/BasicLeve2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer;
+        answer = num1 % num2;
+
+        return answer;
+    }
+}
